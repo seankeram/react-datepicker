a867d8c2f2ad078bbc8bf88b5c86db6a96797136
shayan <shayankeram@gmail.com>
2024-06-26 12:23:36 +0330

    fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,20 @@
-import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import image from '@rollup/plugin-image';
+import resolve from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from "rollup-plugin-postcss";
 import { terser } from "rollup-plugin-terser";
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import image from '@rollup/plugin-image';
 
 const packageJson = require("./package.json");
+const dependencies = ({ dependencies }) => Object.keys(dependencies || {});
+const pkgdependencies = dependencies(pkg);
 
 export default [
     {
         input: "src/index.ts",
+        external: id => pkgdependencies.includes(id),
         output: [
             {
                 file: packageJson.main,
